ca6d3224ecfe5e396bfb55950248f978fabcf2f5
skrasek.michael <skrasek.michal@email.cz>
2022-02-01 22:44:44 +0100

    Makefile, test samples, README, gitignore fix

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Nonlinear Filters
 **Bachelor's thesis**
 
-Topic: Nonlinear denoising filters for 3D filtration (Bilateral filter, Non-local means filter). 
+Topic: Nonlinear filtering for large 3D image data (Bilateral filter, Non-local means filter). 
 
 ## Milestones
 - 2D Bilateral filter (naive implementation) ✔
 - Fast 2D Bilateral filter (custom optimizations) ✔
-- Fast 2D Bilateral filter (aproximation, other optimizaions ...) ❌
-- 2D Non-local means filter (patchwise implementation) ✔
-- 2D Non-local means filter (pixelwise implementation) ✔
-- Fast 2D Non-local means filter (custom optimizaions, integral image ...)
+- Fast 2D Bilateral filter (approximation, other optimization's ...) ❌
+- 2D Non-local means filter (patch wise implementation) ✔
+- 2D Non-local means filter (pixel wise implementation) ✔
+- Fast 2D Non-local means filter (integral image) ✔
+- CLI ✔
 
 ...
 - 3D Bilateral filter
@@ -20,7 +21,7 @@ Topic: Nonlinear denoising filters for 3D filtration (Bilateral filter, Non-loca
 ### Image
 
 - Size: 620x620
-- Depth: 32b grayscale
+- Depth: 32b GrayScale
 - Source: https://www.ipol.im/pub/art/2011/bcm_nlm/article.pdf
 
 ## Bilateral filter
@@ -30,12 +31,28 @@ Topic: Nonlinear denoising filters for 3D filtration (Bilateral filter, Non-loca
 | Spatial sigma | 15    |
 | Range sigma   | 0.1   |
 
-![Bilateral filter](/Images/bilateral.png)
+| Filter variant | Time \[s\]|
+|:---------------|----------:|
+| Bilateral      | 1.8573116 |
+| Fast Bilateral | 0.1328743 |
 
-## Non-local means filter (patchwise)
+![Bilateral filter](/Images/bl-noisy-vs-bilateral.png)
 
-| Parameter | Value |
-|:----------|:-----:|
-| Sigma     | 15    |
+## Non-local means filter
 
-![Non-local means filter](/Images/nonlocal.png)
+| Parameter   | Value |
+|:------------|:-----:|
+| h           | 6     |
+| Patch size  | 3x3   |
+| Window size | 21x21 |
+
+| Filter variant | Time \[s\] |
+|:---------------|-----------:|
+| Pixel wise     | 20.6308629 |
+| Patch wise     | 2.6464456  |
+| Integral image | 2.8232714  |
+
+Pixel wise
+![Non-local means filter](/Images/nlm-noisy-vs-pixel.png)
+Patch wise / Integral image
+![Non-local means filter](/Images/nlm-noisy-vs-patch.png)
---
+++ test.ps1
@@ -0,0 +1,72 @@
+$cli = "Sources/NonlinearFilters.CLI/bin/Release/net6.0/NonlinearFilters.CLI.exe"
+
+function run($params) {
+	Start-Process -FilePath $cli -ArgumentList $params -Wait -NoNewWindow
+}
+
+function run_list($list) {
+	foreach ($param in $list) {
+		run($param)
+		Write-Host ""
+	}
+}
+
+function bilateral {
+	#bilateral (space sigma, range sigma)
+	$params = 
+		"-i Images/noisy.png -o Images/bilateral/bilateral.png -f bf -p `"6, 0.1`"",
+		"-i Images/noisy.png -o Images/bilateral/bilateral-fast.png -f fbf -p `"6, 0.1`""
+
+	run_list($params);
+	join -Width 400 -Cols 2 -Output "Images/bl-noisy-vs-bilateral.png" -Files "Images/noisy.png", "Images/bilateral/bilateral-fast.png"
+}
+
+function nlmeans {
+	#nl-means (patch radius, window radius, h)
+	$params = 
+		"-i Images/noisy.png -o Images/nlmeans/nlmeans-pixel.png -f nlmf -p `"1, 10, 6, 1`"",
+		"-i Images/noisy.png -o Images/nlmeans/nlmeans-patch.png -f nlmf -p `"1, 10, 6, 0`"",
+		"-i Images/noisy.png -o Images/nlmeans/nlmeans-fast.png -f fnlmf -p `"1, 10, 6`""
+
+	run_list($params);
+	join -Width 400 -Cols 2 -Output "Images/nlm-noisy-vs-pixel.png" -Files "Images/noisy.png", "Images/nlmeans/nlmeans-pixel.png"
+	join -Width 400 -Cols 2 -Output "Images/nlm-noisy-vs-patch.png" -Files "Images/noisy.png", "Images/nlmeans/nlmeans-patch.png"
+}
+
+function fnlm_grid_patch_h {
+	$radius = 1, 2, 3
+	$h = 5, 10, 15
+
+	$out = [System.Collections.ArrayList]::new()
+	$params = [System.Collections.ArrayList]::new()
+
+	for ($y = 0; $y -lt $radius.Count; $y++) {
+		for ($x = 0; $x -lt $h.Count; $x++) {
+			$d = $radius[$y] * 2 + 1
+			$out.Add("Images/fnlmf-param-grid/$($d)x$($d)-$($h[$x]).png") | Out-Null
+			#nl-means (patch radius, window radius, h)
+			$params.Add("$($radius[$y]), 17, $($h[$x])") | Out-Null
+		}
+	}
+
+	run("-i Images/noisy.png -o `"$($out -join ',')`" -f fnlmf -t 1 -p `"$($params -join ',')`"")
+
+	join -Width 400 -Cols $h.Count -Output "Images/fnml-param-grid.png" -Files $out
+}
+
+if ((Test-Path $cli -PathType Leaf) -eq $False) {
+	Write-Error "CLI path not found [$cli]"
+	exit
+}
+
+$tests =
+	$Function:bilateral, 
+	$Function:nlmeans, 
+	$Function:fnlm_grid_patch_h
+
+$line = New-Object -TypeName string -ArgumentList '-', 80
+foreach ($test in $tests) {
+	Write-Host $line
+	$test.Invoke()
+}
+Write-Host $line
