00780f71ca2835fc507e62b082a78bb1f66b6a0a
Michael Škrášek <skrasek.michal@email.cz>
2022-02-08 08:54:38 +0100

    OpenCV (#14)
    OpenCV comparison (python implementation)
    Gaussian function fix

---
+++ OpenCV/bl.py
@@ -0,0 +1,24 @@
+import sys
+import cv2 as cv
+import stopwatch
+
+noisyFile = sys.argv[1]
+outputFile = sys.argv[2]
+
+radius = float(sys.argv[3])
+sigmaRange = float(sys.argv[4])
+sigmaSpace = float(sys.argv[5])
+
+sw = stopwatch.stopwatch()
+
+img = cv.imread(noisyFile)
+
+print("Applying OpenCV bilateral filter...", end="")
+sw.start()
+dst = cv.bilateralFilter(img, d=int(2 * radius + 1), sigmaColor=sigmaRange, sigmaSpace=sigmaSpace, borderType=cv.BORDER_ISOLATED)
+sw.stop()
+print("DONE")
+
+cv.imwrite(outputFile, dst)
+print("OpenCV file saved -> " + outputFile)
+print("Time elapsed:", sw.elapsed())
--- Sources/NonlinearFilters/Mathematics/GaussFunction.cs
+++ Sources/NonlinearFilters/Mathematics/GaussFunction.cs
@@ -4,29 +4,27 @@ namespace NonlinearFilters.Mathematics
 {
 	public class GaussFunction
 	{
-		private double expCoeff, coeff;
+		private double expCoeff;
 
 		public void Initalize(double sigma)
 		{
-			coeff = 1 / (sigma * Math.Sqrt(2 * Math.PI));
 			expCoeff = -1 / (2 * sigma * sigma);
 		}
 
 		public double Gauss(double x)
 		{
-			return coeff * Math.Exp(expCoeff * x * x);
+			return Math.Exp(expCoeff * x * x);
 		}
 
 		public Vector4d Gauss(Vector4d v)
 		{
 			Vector4d v2 = v * v;
-			Vector4d u = new(
+			return new Vector4d(
 				Math.Exp(expCoeff * v2.X),
 				Math.Exp(expCoeff * v2.Y),
 				Math.Exp(expCoeff * v2.Z),
 				Math.Exp(expCoeff * v2.W)
 			);
-			return coeff * u;
 		}
 	}
 }
